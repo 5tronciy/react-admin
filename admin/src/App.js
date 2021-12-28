788101cd726f5582fdbcc74fa9dd2edc5d14b055
5tronciy <romangolchuk@gmail.com>
2021-12-28 18:55:19 +0300

    feat: map resource /users

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,10 +1,14 @@
-import { Admin } from "react-admin";
+import { Admin, Resource, ListGuesser } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
 
 const API_URL = "https://jsonplaceholder.typicode.com";
 
 const dataProvider = jsonServerProvider(API_URL);
 
-const App = () => <Admin dataProvider={dataProvider} />;
+const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource name="users" list={ListGuesser} />
+  </Admin>
+);
 
 export default App;
