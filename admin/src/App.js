05a6b2ee22d5836ecdd0ce45b82ba5f213e9ebc3
5tronciy <romangolchuk@gmail.com>
2021-12-29 00:39:47 +0300

    feat: add resource with ReferenceField

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,6 +1,7 @@
 import { Admin, Resource } from "react-admin";
 import jsonServerProvider from "ra-data-json-server";
 import { UserList } from "./users";
+import { PostList } from "./posts";
 
 const API_URL = "https://jsonplaceholder.typicode.com";
 
@@ -8,6 +9,7 @@ const dataProvider = jsonServerProvider(API_URL);
 
 const App = () => (
   <Admin dataProvider={dataProvider}>
+    <Resource name="posts" list={PostList} />
     <Resource name="users" list={UserList} />
   </Admin>
 );
