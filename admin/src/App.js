1f72fabadb00414bb283794156c425f91e269cd9
5tronciy <romangolchuk@gmail.com>
2021-12-29 16:37:18 +0300

    feat: add theme

--- admin/src/App.js
+++ admin/src/App.js
@@ -6,13 +6,27 @@ import { UserList } from "./users";
 import { PostList, PostEdit, PostCreate } from "./posts";
 import Dashboard from "./Dashboard";
 import authProvider from "./authProvider";
+import NotFound from "./NotFound";
+import { createTheme } from "@material-ui/core/styles";
+import { green, purple } from "@mui/material/colors";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: purple[500]
+    },
+    secondary: {
+      main: green[500]
+    }
+  }
+});
 
 const API_URL = "https://jsonplaceholder.typicode.com";
 
 const dataProvider = jsonServerProvider(API_URL);
 
 const App = () => (
-  <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>
+  <Admin theme={theme} dashboard={Dashboard} catchAll={NotFound} authProvider={authProvider} dataProvider={dataProvider}>
     <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
     <Resource name="users" list={UserList} icon={UserIcon} />
   </Admin>
